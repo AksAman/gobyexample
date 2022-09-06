5092995e116c89320835f992362083123299ee57
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-09-06 16:04:24 +0530

    rate limiter done

--- timemachine/ratelimiter.go
+++ timemachine/ratelimiter.go
@@ -2,30 +2,58 @@ package timemachine
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
 type RateLimiter struct {
-	interval time.Duration
-	isBurst  bool
-	ticker   *time.Ticker
-	C        chan time.Time
+	interval   time.Duration
+	burstCount int
+	count      int
+	ticker     *time.Ticker
+	C          chan time.Time
+}
+
+func (rl *RateLimiter) resetBurst() {
+	fmt.Println("---- reset burst")
+	rl.count = rl.burstCount
 }
 
 func (rl *RateLimiter) run() {
-	for {
-		rl.C <- <-rl.ticker.C
+	// if burst count is neg or 0, it is considered as normal rate limiter (pass 1 event every d duration)
+	if rl.burstCount <= 0 {
+		for range rl.ticker.C {
+			rl.C <- time.Now()
+		}
+	} else {
+		// this is a burst limiter
+		// i.e.
+		// every d duration, allows `burstCount` events to pass and then wait for d duration
+
+		// handle if count has become negative or 0
+
+		for {
+			if rl.count <= 0 {
+				// wait till next timer
+				fmt.Println("---- waiting for timer")
+				<-rl.ticker.C
+				rl.resetBurst()
+			}
+
+			rl.C <- time.Now()
+			rl.count--
+		}
+
 	}
 }
 
-func (rl *RateLimiter) Wait() time.Time {
-	return <-rl.C
+func (rl *RateLimiter) Wait() {
+	<-rl.C
 }
 
 func NewRateLimiter(interval time.Duration) (*RateLimiter, error) {
 	rl := &RateLimiter{
 		interval: interval,
-		isBurst:  false,
 		ticker:   time.NewTicker(interval),
 		C:        make(chan time.Time),
 	}
@@ -33,14 +61,20 @@ func NewRateLimiter(interval time.Duration) (*RateLimiter, error) {
 	return rl, nil
 }
 
-func NewBurstyRateLimiter(interval time.Duration, isBurst bool, burstCount int) (*RateLimiter, error) {
+func NewBurstyRateLimiter(interval time.Duration, burstCount int) (*RateLimiter, error) {
 	if burstCount <= 0 {
 		return nil, errors.New("burstCount should be > 0")
 	}
-	rl := &RateLimiter{interval: interval, isBurst: isBurst}
-	rl.C = make(chan time.Time, burstCount)
+
+	rl := &RateLimiter{
+		interval:   interval,
+		ticker:     time.NewTicker(interval),
+		burstCount: burstCount,
+		C:          make(chan time.Time, burstCount),
+	}
 	for i := 0; i < burstCount; i++ {
 		rl.C <- time.Now()
 	}
+	go rl.run()
 	return rl, nil
 }
--- timemachine/ratelimiting.go
+++ timemachine/ratelimiting.go
@@ -12,18 +12,6 @@ type Request struct {
 const N_REQUESTS = 5
 const INTERVAL = time.Millisecond * 1000
 
-func runNormalLimiter() {
-	fmt.Println("Running Normal Rate Limiter with interval: ", INTERVAL)
-
-	// create rate limiter instance
-	if rateLimiter, err := NewRateLimiter(INTERVAL); err != nil {
-		fmt.Printf("err: %v\n", err)
-	} else {
-		sendAndReceiveRequests(N_REQUESTS, rateLimiter)
-	}
-
-}
-
 func sendAndReceiveRequests(n_requests int, rateLimiter *RateLimiter) {
 	// create and send requests
 	requestsChannel := make(chan Request, n_requests)
@@ -38,17 +26,28 @@ func sendAndReceiveRequests(n_requests int, rateLimiter *RateLimiter) {
 		start = time.Now()
 		// wait for rate limit channel
 		rateLimiter.Wait()
-		fmt.Println(time.Since(start), "elapsed")
-		fmt.Printf("request recd: %v\n", request)
+		fmt.Printf("request recd: %v, took: %v\n", request, time.Since(start))
 	}
 }
 
+func runNormalLimiter() {
+	fmt.Println("---- Running Normal Rate Limiter with interval: ", INTERVAL)
+
+	// create rate limiter instance
+	if rateLimiter, err := NewRateLimiter(INTERVAL); err != nil {
+		fmt.Printf("err: %v\n", err)
+	} else {
+		sendAndReceiveRequests(N_REQUESTS, rateLimiter)
+	}
+
+}
+
 func runBurstyLimiter() {
-	fmt.Println("Running Bursty Rate Limiter with interval: ", INTERVAL)
+	fmt.Println("---- Running Bursty Rate Limiter with interval: ", INTERVAL)
 
 	n_requests := N_REQUESTS * 3
 	// create rate limiter instance
-	if rateLimiter, err := NewRateLimiter(INTERVAL); err != nil {
+	if rateLimiter, err := NewBurstyRateLimiter(INTERVAL, 3); err != nil {
 		fmt.Printf("err: %v\n", err)
 	} else {
 		sendAndReceiveRequests(n_requests, rateLimiter)
@@ -58,5 +57,5 @@ func runBurstyLimiter() {
 
 func RunRateLimiting() {
 	runNormalLimiter()
-	// runBurstyLimiter()
+	runBurstyLimiter()
 }
