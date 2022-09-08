9980445e9967fd70b73eadbe5db7d52a07ca0232
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-09-09 03:55:47 +0530

    processes done

--- processes/processes.go
+++ processes/processes.go
@@ -2,14 +2,7 @@ package processes
 
 import (
 	"fmt"
-	"log"
 )
-
-func check(e error) {
-	if e != nil {
-		log.Fatal("error: ", e)
-	}
-}
 
 func Title(s string) {
 	fmt.Printf("\n----- %v -----\n", s)
